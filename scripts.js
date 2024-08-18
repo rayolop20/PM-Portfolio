4534b712b151d39f76826f3eaa328b11acee05c9
Pol Maresch <polmaresch@gmail.com>
2024-08-18 14:36:40 +0200

    header footer added

--- scripts.js
+++
@@ -1,4 +0,0 @@
-function Github_link() { window.open("https://github.com/rayolop20"); }
-function Linkedin_link() { window.open("https://www.linkedin.com/in/pol-maresch-marxuach-1855a9264/") };
-function Mail_link() { window.open("https://www.linkedin.com/in/pol-maresch-marxuach-1855a9264/") };
-function DownloadCV() { window.open("CV/Pol_Maresch_Marxuach_CV_English.pdf") };
